be394a40b402911bcd30629c287c051e8c95e7a1
Qiang Tong <tongqiang.atp@gmail.com>
2015-06-28 16:25:51 -0400

    Refactoring code

--- JavaBasic/src/tests/algorithm/leetcode/MainMethod.java
+++ JavaBasic/src/tests/algorithm/leetcode/MainMethod.java
@@ -4,22 +4,9 @@ public class MainMethod {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		
-		/* Leet Code 228 *******************************
-		 * https://leetcode.com/problems/summary-ranges/
-		 * *********************************************/
-		LC228Test();
-	}
-	
-	public static void LC228Test() {
-		int[][] nums = {
-				{0, 1, 2, 4, 5, 7},
-				{5},
-				{1, 3, 4, 9},
-				{}
-		};
-		for(int[] arr: nums) {
-			ILCSolution lc228 = new LeetCode228(arr);
-			lc228.myPrint();
-		}
+		// https://leetcode.com/problems/summary-ranges/
+		// MySolutionTests.LC228Test();
+		
+		
 	}
 }
--- JavaBasic/src/tests/algorithm/leetcode/MySolutionTests.java
+++ JavaBasic/src/tests/algorithm/leetcode/MySolutionTests.java
@@ -1,5 +1,16 @@
 package tests.algorithm.leetcode;
 
 public class MySolutionTests {
-
+	public static void LC228Test() {
+		int[][] nums = {
+				{0, 1, 2, 4, 5, 7},
+				{5},
+				{1, 3, 4, 9},
+				{}
+		};
+		for(int[] arr: nums) {
+			ILCSolution lc228 = new LeetCode228(arr);
+			lc228.myPrint();
+		}
+	}
 }
