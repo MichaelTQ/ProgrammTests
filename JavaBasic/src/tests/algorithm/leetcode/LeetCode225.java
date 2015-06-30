4480db1917779ffb07e8b10e5b46c7a8518b8029
Qiang Tong <tongqiang.atp@gmail.com>
2015-06-29 23:17:04 -0400

    Adding a better solution
    reference: http://www.geeksforgeeks.org/implement-stack-using-queue/

--- JavaBasic/src/tests/algorithm/leetcode/LeetCode225.java
+++ JavaBasic/src/tests/algorithm/leetcode/LeetCode225.java
@@ -7,49 +7,27 @@ public class LeetCode225 implements ILCSolution {
 	private Queue<Integer> queue2 = new LinkedList<Integer>();
 		
 	public void push(int x) {
-		if(!queue1.isEmpty()) {
-			while(!queue1.isEmpty()) {
-				queue2.add(queue1.poll());
-			}
-			queue2.add(x);
-		} else {
-			while(!queue2.isEmpty()) {
-				queue1.add(queue2.poll());
-			}
-			queue1.add(x);
-		}
+		queue1.add(x);
 	}
 	
 	public void pop() {
-		if(!queue1.isEmpty()) {
-			while(queue1.size() > 1) {
-				queue2.add(queue1.poll());
-			}
-			queue1.poll();
-		} else {
-			while(queue2.size() > 1) {
-				queue1.add(queue2.poll());
-			}
-			queue2.poll();
+		while(queue1.size() > 1) {
+			queue2.add(queue1.poll());
 		}
+		queue1.poll();
+		queue1 = queue2;
+		queue2 = new LinkedList<Integer>();
 	}
 	
 	public int top() {
-		if(!queue1.isEmpty()) {
-			while(queue1.size() > 1) {
-				queue2.add(queue1.poll());
-			}
-			int val = queue1.peek();
+		while(queue1.size() > 1) {
 			queue2.add(queue1.poll());
-			return val;
-		} else {
-			while(queue2.size() > 1) {
-				queue1.add(queue2.poll());
-			}
-			int val = queue2.peek();
-			queue1.add(queue2.poll());
-			return val;
 		}
+		int val = queue1.peek();
+		queue2.add(val);
+		queue1 = queue2;
+		queue2 = new LinkedList<Integer>();
+		return val;
 	}
 	
 	public boolean empty() {
